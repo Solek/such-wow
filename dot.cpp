80f8491af1836a6aacfce15989faec8681332a16
Solek <Solek@users.noreply.github.com>
2016-02-19 13:51:49 +0300

    does it work?

---
+++ dot.cpp
@@ -0,0 +1,11 @@
+// I have forgotten my code at work, so this is done for testing git.
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << "hello, world!" << endl;
+	return 0;
+}
