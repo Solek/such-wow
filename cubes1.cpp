02d908b3ae32f6b87f67f4c04d38fd5e54e452e4
Solek <Solek@users.noreply.github.com>
2016-02-22 04:33:29 +0300

    version 3.0
    Очень сыро, забагованно и не работает. Моего от силы половина. Выложено в качестве направления, идёт тяжело, каждый кусок вымучен. Комментарии и советы приветствуются.

--- cubes1.cpp
+++ cubes1.cpp
@@ -1,36 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	//int cycle;
 	float height;
 	float length;
 	float width;
 	char name[20];
-	int refuse;
 
-	//printf("How many objects do you plan to calculate? ");
-	//scanf("%i", &cycle);
-	//printf("\n");
-	//if (cycle <= 0)
-	//    {
-	//	printf("Okay, bye.");
-	//	return 0;
-	//    }
 
+
+	FILE * input;
+	input = fopen("input.txt", "rt");
+
+	// Что-то нашёл и утащил, что-то правил, чистый си сам не смог реализовать.
+	// Посимвольное считывание в char[] было самым многообещающим, но нет.
+	// number_lines используется для подсчёта линий в файле.
+
+	// Основывается это на том, что на каждый объект вводится стабильное число строк + одна пустая.
+	// В случае отсутствия данных шаблон для обозначения отсутствующих данных должен быть на месте.
+
+    char *line_check = new char [30];
+    int number_lines = 0;
+    ifstream base("input.txt");
+    while (!base.eof())
+    	{
+        base.getline(line_check, 30, '\n');
+        number_lines++;
+    	}
+    base.close();
+    delete line_check;
+
+    printf("Number of lines in input file is %i.\n", number_lines);
+    printf("Quantity of objects is %i.\n\n", (number_lines-1)/5); // по 5 строк, включая пробел для разделения минус нулевая строка.
+
+    //Хотел по-хитрому, не осилил переходы на новую строку через чисто си.
+    /*char str[20];
+    for (int i = 0; i < number_lines; i++)
+    	{
+    	fscanf(input, "%[^\n]", str);
+    	printf(str);
+    	}
+	*/
+
+	const int len = 30;
+	const char ch = '\n';
+	char mass[len][number_lines];
+	char mass2[len - 8][number_lines];
+	char mass3[len - 8][number_lines];
+
+
+	ifstream file("input.txt", ios::in | ios::binary);
+
+	if(!file) return 1;
+
+	for (int r = 0; r < number_lines; r++)
+	{
+		file.getline(mass[r], len-1,ch); //Считываем строки в массив
+		cout << "String " << r+1 << " = "<< mass[r] << endl; //Выводим сырую строку из массива
+		// переработка? scanf не тянет. выдает не то, что ожидается.
+
+		/*if (int isdigit(int mass2[r]))
+			{
+			mass3[r] = atof(mass2[r]);
+			}*/
+
+	// + удалить пустые строки между данными
+
+	}
+
+	// здесь уже по идее можно считать, но пока нечего :)
+
+
+
+
+/*
     FILE * output;
-    if ((output = fopen("result.txt", "wt")) == 0) // открытие с проверкой на успешность выполнения
+    if (output = fopen("result.txt", "wt") == 0) // открытие с проверкой на успешность выполнения
         {
-    	printf("Error with opening or creating file!");
+    	printf("Error with opening or creating output file!");
     	return 0;
         }
 
-    int i = 0; // пронумеровка вводимых объектов
-    int j = 0; // хак для прерывания
 
-    while (j < 1)
-        {
-			printf("Input name of your %i object: ", i + 1); // на пробелы забил, запутался
+
+			printf("Input name of your %i object: ", i + 1);
 			scanf("%20s", &name);
 			printf("\n");
 
@@ -43,7 +102,7 @@ int main()
 			printf("\n");
 
 			printf("Input height of your object: ");
-			scanf("%f", &height);   // узнать подробнее про значок & и как не дать сломать вводом букв
+			scanf("%f", &height);   // узнать подробнее про значок & и как не дать сломать вводом букв. %[0-9] ?
 			printf("\n");
 
 			fprintf(output, "Name of object: \"%s\". Length is %.3f, width is %.3f, height is %.3f.\n", name, length, width, height);
@@ -54,19 +113,9 @@ int main()
 			else
 				fprintf(output, "Object \"%s\" doesn't exist!\n\n", name);
 
-			printf("Do you want to input one more object?\n");
-		    printf("Type \"0\" if you are done with input or any positive number if not. ");
-			scanf("%d", &refuse);
+*/
+    file.close();
+    //fclose(output);
 
-			if (refuse > 0)  // убогий костыль, но хоть работает. нужно разбираться
-				j = 0;
-
-			else
-				j++;
-
-			i++;
-		}
-
-    fclose(output);
     return 0;
 }
