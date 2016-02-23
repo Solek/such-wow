4a664912ccc957dccbb2be731afe446c359351e2
Solek <Solek@users.noreply.github.com>
2016-02-24 02:48:48 +0300

    Manual pointing for i/o files.
    Also, only smallest object in  output file. Wanted to save a line in cycle, but i've got stuck. Anyway, tons of variables are bad, but it works. Kinda.

--- cubes1.cpp
+++ cubes1.cpp
@@ -9,40 +9,46 @@ using namespace std;
 int main()
 {
 	float length, width, height, total;
-	float prev_total = 10000000;
+	float length1, width1, height1;
+	float min_total = 10000000;
 	char name[20];
-	char name_smallest[20];
+	char name1[20];
 	char temp[40];
+	char filename_i[256];
+	char filename_o[256];
+
+	printf("Type name of your file please: \n");
+	std::cin.getline(filename_i, 256);
+	printf("\nThanks! Now, how your output file should be called? \n");
+	std::cin.getline(filename_o, 256);
+	printf("\n");
+
 
 	FILE * input;
-	input = fopen("input.txt", "rt");
+	input = fopen(filename_i, "rt");
     FILE * output;
-    output = fopen("result.txt", "wt");
+    output = fopen(filename_o, "wt");
 
     while (fgets(temp, 200, input) != NULL)
     	{
     	std::stringstream ss_line;
+
     	ss_line << temp;
     	ss_line >> name >> length >> width >> height;
     	std::cout << name << " " << length << " " << width << " " << height <<'\n';
-
-        if (length > 0 && width > 0 && height > 0)
-        {
-
-        	fprintf(output, "Name of object: \"%s\". Length is %.3f, width is %.3f, height is %.3f.\n", name, length, width, height);
-        	total = 2 * (length * height + length * width + height * width);
-        	if (total < prev_total)
-        		{
-        		prev_total = total;
-        		strcpy(name_smallest, name);
-        		}
-        	fprintf(output, "The total square of your object \"%s\" is %.3f.\n\n", name, total);
-        }
-        else
-        	fprintf(output, "Object \"%s\" doesn't exist!\n\n", name);
+    	total = 2 * (length * height + length * width + height * width);
+        if (length > 0 && width > 0 && height > 0 && total < min_total)
+        	{
+        	strcpy(name1, name);
+        	length1 = length;
+        	width1 = width;
+        	height1 = height;
+        	min_total = total;
+        	}
     	}
 
-    fprintf(output, "Object \"%s\" has the smallest square %.3f!\n\n", name_smallest, prev_total);
+    fprintf(output, "Smallest square between all existing objects is %.3f!\n\n", min_total);
+    fprintf(output, "Name of object is \"%s\". Length is %.3f, width is %.3f and height is %.3f.\n", name1, length1, width1, height1);
 
     fclose(input);
     fclose(output);
